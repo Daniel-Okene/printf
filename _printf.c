dc0d24d473f0be217426b3744659696349b5484d
Daniel-Okene <simplydaniel11@gmail.com>
2023-03-25 10:46:46 -0700

    finished the _printf function

--- _printf.c
+++ _printf.c
@@ -13,13 +13,54 @@ int _printf(const char *format, ...)
 	va_list list;
 	char buffer[BUFF_SIZE];
 
+	/* checking if format is NULL */
+	if (format == NULL)
+		return (-1);
+
 	va_start(list, format);
+
+	/* reading the inputed format specifier */
 	for (i = 0; format && &format[i] != '\0'; i++)
 	{
+		/* checking for the starting point of a format specifier */
 		if (format[i] != '%')
-		{}
-		printed++;
+		{
+			/* storing the format specifier in the buffer */
+			buffer[buff_ind++] = format[i];
+			/* checking for buffer overfloe */
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &1);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
 
-	va_end(list, format);
+	pritn_buffer(buffer, &buff_ind);
+	va_end(list);
+	return (printed_cahrs);
+}
+
+/**
+ * prints the contents of the buffer if it existd
+ * @buffer: array of chars
+ * @buff_ind: index at which to add next char, represents the length
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
 }
