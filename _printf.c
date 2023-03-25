3eb4bc2935ca57a9950da66e45d47c1844e03e40
imperial-coder <okenedaniel66@gmail.com>
2023-03-24 17:41:01 -0700

    added the main file _printf.c

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#inlcude "main.h"
+
+/**
+ * _printf - a clone of the standard printf function
+ * @format: conversion specifiers
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, printed_char = 0;
+	va_list list;
+
+	va_start(list, format);
+}
